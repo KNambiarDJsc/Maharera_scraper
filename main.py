54da93a5504372f5c7c3ca38f4349b61b7131200
KNambiarDJsc <knumbiar@gmail.com>
2025-11-24 12:24:57 +0530

    extract building details

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import csv
 import pandas as pd
 from playwright.async_api import async_playwright, Playwright, Browser, BrowserContext, Page
 from modules.captcha_solver import CaptchaSolver
-from modules.data_extracter import DataExtracter
+from modules.data_extractor import DataExtracter
 from typing import Set, Optional
 
 # --- Configuration ---
